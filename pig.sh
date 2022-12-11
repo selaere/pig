141506daeb4a9d204dfc1258fad36a4b4ec36fbb
taswelll <12009104+selaere@users.noreply.github.com>
2022-12-11 22:31:15 +0100

    i am dumb

--- pig.sh
+++ pig.sh
@@ -1,15 +1,23 @@
 # !/usr/bin/env bash
 
-mkdir -p tmp
+mkdir -p /tmp/pig
 mkdir -p log
-[ -e tmp/in  ] && rm tmp/in
-[ -e tmp/out ] && rm tmp/out
-mkfifo tmp/in
-mkfifo tmp/out
+[ -e /tmp/pig/in  ] && rm /tmp/pig/in
+[ -e /tmp/pig/out ] && rm /tmp/pig/out
+mkfifo /tmp/pig/in
+mkfifo /tmp/pig/out
 logfile=$(date +log/%F.pig.log)
-netcat -q 10 irc.osmarks.net 6667 < tmp/in > tmp/out &
+(
+while true; do 
+    (
+        printf ":pig.sh STARTPLZ\r\n" > /tmp/pig/out
+        # echo "finished" >2
+    ) &
+    netcat -q -1 -w 10 mondecitronne.com 6667 < /tmp/pig/in > /tmp/pig/out
+done
+) &
 # change "irc.osmarks.net" to the server address
 # "-q 10" in my netcat version tells it to wait before writing, you might need something else
 # (see <https://superuser.com/q/261900>)
-printf ":pig.sh STARTPLZ\r\n" > tmp/out &
-$bqn pig.bqn run > tmp/in < tmp/out 2> >(tee -a $logfile >&2)
+# printf ":pig.sh STARTPLZ\r\n" > /tmp/pig/out &
+$bqn pig.bqn run > /tmp/pig/in < /tmp/pig/out 2> >(tee -a $logfile >&2)
