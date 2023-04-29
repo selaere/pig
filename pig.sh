0c3b264b39ccfab538d225f339b6af299035f1e2
selaere <selaere>
2023-04-29 20:26:49 +0200

    tweak script

--- pig.sh
+++ pig.sh
@@ -13,11 +13,11 @@ while true; do
         printf ":pig.sh STARTPLZ\r\n" > /tmp/pig/out
         # echo "finished" >2
     ) &
-    netcat -q -1 -w 10 mondecitronne.com 6667 < /tmp/pig/in > /tmp/pig/out
+    # change "irc.osmarks.net" to the server address
+    # "-w 10" in my netcat version tells it to wait before writing, you might need something else
+    # (see <https://superuser.com/q/261900>)
+    netcat -w 60 irc.osmarks.net 6667 < /tmp/pig/in > /tmp/pig/out
 done
 ) &
-# change "irc.osmarks.net" to the server address
-# "-q 10" in my netcat version tells it to wait before writing, you might need something else
-# (see <https://superuser.com/q/261900>)
 # printf ":pig.sh STARTPLZ\r\n" > /tmp/pig/out &
 $bqn pig.bqn run > /tmp/pig/in < /tmp/pig/out 2> >(tee -a $logfile >&2)
