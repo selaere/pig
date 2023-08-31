9de74431f19be3c7a951ac904a513eea38b8d1ed
selaere <12009104+selaere@users.noreply.github.com>
2023-09-01 00:13:12 +0200

    new IRCParse

--- pig.sh
+++ pig.sh
@@ -7,12 +7,12 @@ mkdir -p log
 mkfifo /tmp/pig/in
 mkfifo /tmp/pig/out
 logfile=$(date +log/%F.pig.log)
-( while true; do
+( if true; then
     printf ":pig.sh STARTPLZ\r\n" &
     # change "irc.osmarks.net" to the server address
     # "-w 60" in my netcat version tells it to wait before writing, you might need something else
     # (see <https://superuser.com/q/261900>)
     netcat -w 60 irc.osmarks.net 6667
-done ) < /tmp/pig/in > /tmp/pig/out &
+fi ) < /tmp/pig/in > /tmp/pig/out &
 # printf ":pig.sh STARTPLZ\r\n" > /tmp/pig/out &
 $bqn pig.bqn run > /tmp/pig/in < /tmp/pig/out 2> >(tee -a $logfile >&2)
